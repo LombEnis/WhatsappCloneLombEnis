f529480bb78e4ee3ce2447cdf4d8b6f29e42066d
Enisssss <b.encio.003@gmail.com>
2021-04-25 00:45:31 +0200

    added TabLayout and ViewPager

--- app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
+++ app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.whatsappclonelombenis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout= findViewById(R.id.tabLayout);
+        viewPager= findViewById(R.id.pager);
+
     }
 }
