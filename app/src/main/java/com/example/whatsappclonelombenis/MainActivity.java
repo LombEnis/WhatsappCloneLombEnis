f863e149b7b918e9a0b66831be87e54cd6abe1cb
Matteo Lombardi <lombardi.matteo484@gmail.com>
2021-04-26 00:28:26 +0200

    Set action bar

--- app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
+++ app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.whatsappclonelombenis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
