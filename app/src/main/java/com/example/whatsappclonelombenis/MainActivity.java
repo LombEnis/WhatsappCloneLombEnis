92d9596d59b1a43cd94d5c9365dd0140b4dd80aa
Enisssss <b.encio.003@gmail.com>
2021-04-27 00:30:44 +0200

    added tabs weight

--- app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
+++ app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 import com.google.android.material.tabs.TabLayout;
@@ -28,6 +29,26 @@ public class MainActivity extends AppCompatActivity {
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
         viewPager.setCurrentItem(1);
+
+        LinearLayout layout0 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0));
+        LinearLayout.LayoutParams layoutParams0 = (LinearLayout.LayoutParams) layout0.getLayoutParams();
+        layoutParams0.weight = 1;
+        layout0.setLayoutParams(layoutParams0);
+
+        LinearLayout layout1 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(1));
+        LinearLayout.LayoutParams layoutParams1 = (LinearLayout.LayoutParams) layout1.getLayoutParams();
+        layoutParams1.weight = 2;
+        layout1.setLayoutParams(layoutParams1);
+
+        LinearLayout layout2 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(2));
+        LinearLayout.LayoutParams layoutParams2 = (LinearLayout.LayoutParams) layout2.getLayoutParams();
+        layoutParams2.weight = 2;
+        layout2.setLayoutParams(layoutParams2);
+
+        LinearLayout layout3 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(3));
+        LinearLayout.LayoutParams layoutParams3 = (LinearLayout.LayoutParams) layout3.getLayoutParams();
+        layoutParams3.weight = 2;
+        layout3.setLayoutParams(layoutParams3);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
