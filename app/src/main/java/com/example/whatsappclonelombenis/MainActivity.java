b2d161c25003bf74a4073e3eeae49a94a8c9cb93
Matteo Lombardi <lombardi.matteo484@gmail.com>
2021-04-27 23:36:54 +0200

    Perfected ActionBar and Font

--- app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
+++ app/src/main/java/com/example/whatsappclonelombenis/MainActivity.java
@@ -1,22 +1,18 @@
 package com.example.whatsappclonelombenis;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.MenuItemCompat;
 
-import android.app.ActionBar;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SearchView;
-import android.widget.Toast;
 
 
 import com.google.android.material.tabs.TabLayout;
@@ -25,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
     // TabLayout
     private TabLayout tabLayout;
     private ViewPager viewPager;
-  
+
     // ActionBar
     private Toolbar toolbar;
     private View filters;
+
+    private androidx.appcompat.widget.SearchView searchView;
 
     private MenuItem filterButtonItem;
     private Button filterButton;
@@ -37,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      
+
         // ActionBar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,10 +43,10 @@ public class MainActivity extends AppCompatActivity {
         filters = findViewById(R.id.filters);
 
         // TabLayout creation
-        tabLayout= findViewById(R.id.tabLayout);
+        tabLayout = findViewById(R.id.tabLayout);
 
-        viewPager= findViewById(R.id.pager);
-        PagerAdapter pagerAdapter= new PagerAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.pager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
@@ -58,22 +56,22 @@ public class MainActivity extends AppCompatActivity {
         // Setting Tabs weight
         LinearLayout layout0 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0));
         LinearLayout.LayoutParams layoutParams0 = (LinearLayout.LayoutParams) layout0.getLayoutParams();
-        layoutParams0.weight = 1;
+        layoutParams0.weight = 3;
         layout0.setLayoutParams(layoutParams0);
 
         LinearLayout layout1 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(1));
         LinearLayout.LayoutParams layoutParams1 = (LinearLayout.LayoutParams) layout1.getLayoutParams();
-        layoutParams1.weight = 2;
+        layoutParams1.weight = 8;
         layout1.setLayoutParams(layoutParams1);
 
         LinearLayout layout2 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(2));
         LinearLayout.LayoutParams layoutParams2 = (LinearLayout.LayoutParams) layout2.getLayoutParams();
-        layoutParams2.weight = 2;
+        layoutParams2.weight = 8;
         layout2.setLayoutParams(layoutParams2);
 
         LinearLayout layout3 = ((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(3));
         LinearLayout.LayoutParams layoutParams3 = (LinearLayout.LayoutParams) layout3.getLayoutParams();
-        layoutParams3.weight = 2;
+        layoutParams3.weight = 8;
         layout3.setLayoutParams(layoutParams3);
 
         // Setting tab listener
@@ -81,13 +79,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+
+                filters.setVisibility(View.GONE);
+                tabLayout.setVisibility(View.VISIBLE);
+                searchView.setIconified(true);
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
         });
     }
 
@@ -95,27 +99,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_options_menu, menu);
 
+        filterButtonItem = menu.findItem(R.id.app_bar_filter_button);
+        filterButton = filterButtonItem.getActionView().findViewById(R.id.filter_button);
+
         // Declare search item
         MenuItem searchItem = menu.findItem(R.id.app_bar_search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView = (androidx.appcompat.widget.SearchView) searchItem.getActionView();
+        searchView.setQueryHint(getResources().getString(R.string.cerca));
+
+        View closeSearchViewButton = searchView.findViewById(R.id.search_close_btn);
 
         // Set listeners for search item
         searchView.setOnSearchClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 // Search view is expanded
                 filters.setVisibility(View.VISIBLE);
-            }
-        });
-
-        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
-            @Override
-            public boolean onClose() {
-                // When search view is collapsed
-                filters.setVisibility(View.GONE);
-                filterButtonItem.setVisible(false);
-                return false;
+                tabLayout.setVisibility(View.GONE);
             }
         });
 
@@ -129,12 +129,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 // For auto complete search process
+                if (searchView.getQuery().length() != 0) {
+                    filters.setVisibility(View.GONE);
+                } else {
+                    filters.setVisibility(View.VISIBLE);
+                }
                 return false;
             }
         });
 
-        filterButtonItem = menu.findItem(R.id.app_bar_filter_button);
-        filterButton = filterButtonItem.getActionView().findViewById(R.id.filter_button);
+        closeSearchViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (searchView.getQuery().length() != 0 ||
+                        filterButtonItem.isVisible()) {
+                    searchView.setQuery("", false);
+                    filterButtonItem.setVisible(false);
+                    filters.setVisibility(View.VISIBLE);
+                } else {
+                    searchView.onActionViewCollapsed();
+                    filters.setVisibility(View.GONE);
+                    tabLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
     }
