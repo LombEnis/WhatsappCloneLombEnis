7a729099abbbe1131a5f8fb533811e6fb255fe09
Enisssss <b.encio.003@gmail.com>
2021-04-29 15:43:31 +0200

    added recycler view custom layout and initial adapter(didn't move files into staging area previously

--- app/src/main/java/com/example/whatsappclonelombenis/ChatRecViewAdapter.java
+++ app/src/main/java/com/example/whatsappclonelombenis/ChatRecViewAdapter.java
@@ -1,4 +1,37 @@
 package com.example.whatsappclonelombenis;
 
-public class ChatRecViewAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChatRecViewAdapter extends RecyclerView.Adapter<ChatRecViewAdapter.ViewHolder>{
+    private Context context;
+
+    public ChatRecViewAdapter(Context context) {this.context=context;}
+
+    @NonNull
+    @Override
+    public ChatRecViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatRecViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
