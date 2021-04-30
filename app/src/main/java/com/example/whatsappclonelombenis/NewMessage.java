a97c153445cc93ad2ac98564a01b9464a31e42ff
Enisssss <b.encio.003@gmail.com>
2021-04-30 23:37:54 +0200

    added toolbar to new activity

--- app/src/main/java/com/example/whatsappclonelombenis/NewMessage.java
+++ app/src/main/java/com/example/whatsappclonelombenis/NewMessage.java
@@ -1,14 +1,26 @@
 package com.example.whatsappclonelombenis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class NewMessage extends AppCompatActivity {
+    Toolbar newMessageToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        newMessageToolbar=findViewById(R.id.newMessageActivityToolbar);
+        setSupportActionBar(newMessageToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.newmessage_menu, menu);
+        return true;
     }
 }
