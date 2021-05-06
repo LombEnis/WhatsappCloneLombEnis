1a0fbd4eede8d57ffcc1b4ecd257cd0e5c97b50f
LoMbiChh <lombardi.matteo484@gmail.com>
2021-05-06 23:52:05 +0200

    Insert values for the layout of the first story of StoriesActivity

--- app/src/main/java/com/example/whatsappclonelombenis/StoriesActivity.java
+++ app/src/main/java/com/example/whatsappclonelombenis/StoriesActivity.java
@@ -6,27 +6,72 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class StoriesActivity extends AppCompatActivity {
-    // ActionBar
+    // Layout view
+    private RelativeLayout rootLayout;
+    private LinearLayout progressLinearLayout;
     private Toolbar actionBar;
+    private ImageView backgroundImageView;
+    private TextView mainTextView;
+    private Button replyButton;
 
+    // Contacts
     private ArrayList<Contact> contacts;
+    private int currentContactPos;
+    private Contact currentContact;
+
+    private Story currentStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
 
-        // Instantiate ActionBar variables
+        // Instantiate layout view
+        rootLayout = findViewById(R.id.root_layout);
+        progressLinearLayout = findViewById(R.id.progress_linearlayout);
         actionBar = findViewById(R.id.action_bar);
+        backgroundImageView = findViewById(R.id.background_imageview);
+        mainTextView = findViewById(R.id.main_textview);
+        replyButton = findViewById(R.id.reply_button);
+
 
         // Set ActionBar
         setSupportActionBar(actionBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        // Set current contact
+        contacts = StatusRecViewAdapter.contacts;
+
+        currentContactPos = getIntent().getIntExtra("position", -1);
+        currentContact = contacts.get(currentContactPos);
+
+        // Get current contact attributes
+        currentStory = currentContact.getStatusStories().get(0);
+
+        // Set story values in the layout
+        rootLayout.setBackgroundColor(getResources().getColor(currentStory.getBackgroundColor()));
+
+        Glide.with(this)
+                .load(currentStory.getBackgroundImage())
+                .into(backgroundImageView);
+
+        mainTextView.setTextColor(getResources().getColor(currentStory.getMainTextColor()));
+        mainTextView.setText(currentStory.getMainTextString());
+
+        // Create progress bars
+
     }
 
     @Override
