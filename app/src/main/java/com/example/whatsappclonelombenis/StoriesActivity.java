77ced1914f1f743b2d6db5def76f3abd704f3401
LoMbiChh <lombardi.matteo484@gmail.com>
2021-05-11 22:49:26 +0200

    Set fullscreen mode to the StoriesActivity

--- app/src/main/java/com/example/whatsappclonelombenis/StoriesActivity.java
+++ app/src/main/java/com/example/whatsappclonelombenis/StoriesActivity.java
@@ -3,15 +3,19 @@ package com.example.whatsappclonelombenis;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -21,7 +25,7 @@ import java.util.ArrayList;
 
 public class StoriesActivity extends AppCompatActivity {
     // Layout view
-    private RelativeLayout rootLayout;
+    private RelativeLayout rootRelativeLayout;
     private LinearLayout progressLinearLayout;
     private Toolbar actionBar;
     private ImageView backgroundImageView;
@@ -39,13 +43,16 @@ public class StoriesActivity extends AppCompatActivity {
     private Button leftButton;
     private Button rightButton;
 
+
+    private int statusBarHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
 
         // Instantiate layout view
-        rootLayout = findViewById(R.id.root_layout);
+        rootRelativeLayout = findViewById(R.id.root_layout);
         progressLinearLayout = findViewById(R.id.progress_linearlayout);
         actionBar = findViewById(R.id.action_bar);
         backgroundImageView = findViewById(R.id.background_imageview);
@@ -60,6 +67,41 @@ public class StoriesActivity extends AppCompatActivity {
         setSupportActionBar(actionBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // Delete status bar and expand the layout
+        getWindow().setDecorFitsSystemWindows(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController controller = getWindow().getInsetsController();
+
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars());
+                controller.hide(WindowInsets.Type.navigationBars());
+            }
+
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        } else {
+            //noinspection deprecation
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
+
+
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+
+        // Set top margin for the action bar
+        RelativeLayout.LayoutParams actionBarLayoutParams = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        actionBarLayoutParams.setMargins(0, statusBarHeight + 5, 0, 0);
+        actionBar.setLayoutParams(actionBarLayoutParams);
 
         // Set current contact
         contacts = StatusRecViewAdapter.contacts;
@@ -127,12 +169,45 @@ public class StoriesActivity extends AppCompatActivity {
     }
 
     private void setCurrentStoryLayout() {
-        rootLayout.setBackgroundColor(currentStory.getBackgroundColorResource());
+        // Create progress bars
+        int statusStoriesSize = currentContact.getStatusStories().size();
+        for (int i = 0; i < statusStoriesSize; i++) {
+            ProgressBar progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+
+            LinearLayout.LayoutParams progressBarLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, 3, 1);
+
+            if (i == 0) {
+                progressBarLayoutParams.setMargins(10, statusBarHeight, 5, 0);
+            } else if (i == (statusStoriesSize - 1)) {
+                progressBarLayoutParams.setMargins(5, statusBarHeight, 10, 0);
+            } else {
+                progressBarLayoutParams.setMargins(5, statusBarHeight, 5, 0);
+            }
+
+            progressBar.setLayoutParams(progressBarLayoutParams);
+            progressBar.setBackgroundColor(getResources().getColor(R.color.transparent_grey));
+            progressBar.setMax(2000);
+
+            progressLinearLayout.addView(progressBar);
+        }
+
+        rootRelativeLayout.setBackgroundColor(currentStory.getBackgroundColorResource());
 
         Glide.with(this)
                 .load(currentStory.getBackgroundImageString())
                 .into(backgroundImageView);
 
         mainTextView.setText(currentStory.getMainTextString());
+    }
+
+    public void startStory() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        thread.start();
     }
 }
--- app/src/main/java/com/example/whatsappclonelombenis/TabChatFragment.java
+++ app/src/main/java/com/example/whatsappclonelombenis/TabChatFragment.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,80 +53,65 @@ public class TabChatFragment extends Fragment {
 
         sampleBackgroundImageView = view.findViewById(R.id.sample_background_imageview);
 
-        // Execute AsyncTask for setting the imageview and taking the bitmap from the layout, then putting it in the stories of the contacts
-        CreateContactsTask createContactsTask = new CreateContactsTask();
-        createContactsTask.execute();
+        // Execute new thread for setting the imageview and taking the bitmap from the layout, then putting it in the stories of the contacts
+
+        Thread createContactsThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url;
+                try {
+                    url = new URL(sampleImageUrlString);
+                    Bitmap sampleImage = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            sampleBackgroundImageView.setImageBitmap(sampleImage);
+
+                            ArrayList<Contact> contacts = new ArrayList<>();
+
+                            ArrayList<Story> sampleStatusStories = new ArrayList<Story>();
+
+                            sampleStoryRelativeLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                                @Override
+                                public void onGlobalLayout() {
+                                    sampleStoryRelativeLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                                    sampleStoryRelativeLayout.setDrawingCacheEnabled(true);
+                                    sampleStoryRelativeLayout.buildDrawingCache();
+                                    Bitmap sampleBitmap = sampleStoryRelativeLayout.getDrawingCache();
+
+                                    Story firstStory = new Story(new Date(), ContextCompat.getColor(getContext(), R.color.orchid), sampleImageUrlString,
+                                            "Storia 1", "Questo è un caption", sampleBitmap);
+
+                                    Story secondStory = new Story(new Date(), ContextCompat.getColor(getContext(), R.color.orchid), sampleImageUrlString,
+                                            "Storia 2", "Questo è un caption", sampleBitmap);
+
+                                    sampleStatusStories.add(firstStory);
+                                    sampleStatusStories.add(secondStory);
+
+                                    contacts.add(new Contact("Leonardo DiCaprio", "3666875674", sampleImageUrlString,
+                                            "Questo è il mio stato", sampleStatusStories));
+                                    contacts.add(new Contact("Leonardo DiCaprio", "3666875674", sampleImageUrlString,
+                                            "Questo è il mio stato", sampleStatusStories));
+                                    contacts.add(new Contact("Leonardo DiCaprio", "3666875674", sampleImageUrlString,
+                                            "Questo è il mio stato", sampleStatusStories));
+
+
+                                    TabStatusFragment.recViewAdapter.setContacts(contacts);
+                                }
+                            });
+                        }
+                    });
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        createContactsThread.start();
 
         return view;
-    }
-
-
-    public class CreateContactsTask extends AsyncTask<String, String, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            URL url;
-            try {
-                url = new URL(sampleImageUrlString);
-                Bitmap sampleImage = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        sampleBackgroundImageView.setImageBitmap(sampleImage);
-
-                        ArrayList<Contact> contacts = new ArrayList<>();
-
-                        ArrayList<Story> sampleStatusStories = new ArrayList<Story>();
-
-                        sampleStoryRelativeLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                            @Override
-                            public void onGlobalLayout() {
-                                sampleStoryRelativeLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-
-                                sampleStoryRelativeLayout.setDrawingCacheEnabled(true);
-                                sampleStoryRelativeLayout.buildDrawingCache();
-                                Bitmap sampleBitmap = sampleStoryRelativeLayout.getDrawingCache();
-
-                                Story firstStory = new Story(new Date(), ContextCompat.getColor(getContext(), R.color.black), sampleImageUrlString,
-                                        "Storia 1", "Questo è un caption", sampleBitmap);
-
-                                Story secondStory = new Story(new Date(), ContextCompat.getColor(getContext(), R.color.orchid), sampleImageUrlString,
-                                        "Storia 2", "Questo è un caption", sampleBitmap);
-
-                                sampleStatusStories.add(firstStory);
-                                sampleStatusStories.add(secondStory);
-
-                                contacts.add(new Contact("Leonardo DiCaprio", "3666875674", sampleImageUrlString,
-                                        "Questo è il mio stato", sampleStatusStories));
-                                contacts.add(new Contact("Leonardo DiCaprio", "3666875674", sampleImageUrlString,
-                                        "Questo è il mio stato", sampleStatusStories));
-                                contacts.add(new Contact("Leonardo DiCaprio", "3666875674", sampleImageUrlString,
-                                        "Questo è il mio stato", sampleStatusStories));
-
-
-                                TabStatusFragment.recViewAdapter.setContacts(contacts);
-                            }
-                        });
-                    }
-                });
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-        }
     }
 }
